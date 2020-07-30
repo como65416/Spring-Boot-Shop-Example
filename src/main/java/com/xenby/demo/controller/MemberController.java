ab7c4c40443b9b3a758b9cba21b22fac0fca5a36
comoco <como65416@gmail.com>
2020-07-30 22:40:04 +0800

    add login form example

--- src/main/java/com/xenby/demo/controller/MemberController.java
+++ src/main/java/com/xenby/demo/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.xenby.demo.controller;
 
+import com.xenby.demo.form.LoginForm;
 import com.xenby.demo.service.JwtService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,23 +17,21 @@ public class MemberController {
     JwtService jwtService;
 
     @ApiOperation("登入")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "body", value = "登入資料", required = true, dataType = "String", paramType = "body")
-    })
     @ApiResponses({
             @ApiResponse(code = 200, message = "登入成功"),
             @ApiResponse(code = 401, message = "登入失敗")
     })
     @PostMapping(value="/login")
-    public ResponseEntity<Map<String, Object>> deleteStudent(@RequestBody Map<String, Object> body) {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginForm loginForm) {
         Map<String, Object> map = new HashMap<String, Object>();
-        if (!body.get("username").equals("user") || !body.get("password").equals("1234")) {
+        if (!loginForm.getUsername().equals("user") || !loginForm.getPassword().equals("1234")) {
             map.put("message", "Username or Password not validated");
             return new ResponseEntity<Map<String,Object>>(map, HttpStatus.UNAUTHORIZED);
         }
 
         Map<String, Object> data = new HashMap<String, Object>();
         map.put("token", jwtService.generateToken(data));
+
         return new ResponseEntity<Map<String,Object>>(map, HttpStatus.OK);
     }
 }
