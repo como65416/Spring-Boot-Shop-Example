06fd7cb45b983a5f86ade7c75f9ebda269765259
comoco <como65416@gmail.com>
2020-08-03 23:26:10 +0800

    add User model and repository

---
+++ src/main/java/com/xenby/demo/model/User.java
@@ -0,0 +1,57 @@
+package com.xenby.demo.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Table(name = "user")
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", insertable = false, nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
