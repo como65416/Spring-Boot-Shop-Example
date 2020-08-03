d7549783a5b72555f1fbf9c255dd644495cf1579
comoco <como65416@gmail.com>
2020-08-03 23:26:42 +0800

    use bcrypt example

--- src/main/java/com/xenby/demo/controller/MemberController.java
+++ src/main/java/com/xenby/demo/controller/MemberController.java
@@ -3,11 +3,14 @@ package com.xenby.demo.controller;
 import com.xenby.demo.exception.UnauthorizedException;
 import com.xenby.demo.form.LoginForm;
 import com.xenby.demo.model.JwtClaimsData;
+import com.xenby.demo.model.User;
+import com.xenby.demo.repository.UserRepository;
 import com.xenby.demo.service.JwtService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,6 +22,9 @@ public class MemberController {
     @Autowired
     JwtService jwtService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @ApiOperation("登入")
     @ApiResponses({
             @ApiResponse(code = 200, message = "登入成功"),
@@ -28,13 +34,16 @@ public class MemberController {
     @PostMapping(value="/login")
     public Map<String, Object> login(@Valid @RequestBody LoginForm loginForm) throws Exception {
         Map<String, Object> map = new HashMap<String, Object>();
-        if (!loginForm.getPassword().equals("1234")) {
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        User user = userRepository.findByUsername(loginForm.getUsername());
+        if (user == null || !bCryptPasswordEncoder.matches(loginForm.getPassword(), user.getPassword())) {
             throw new UnauthorizedException("Username or Password not validated");
         }
 
         JwtClaimsData jwtClaimsData = new JwtClaimsData();
-        jwtClaimsData.setUsername(loginForm.getUsername());
-        jwtClaimsData.setRole(loginForm.getUsername().equals("admin") ? "admin" : "member");
+        jwtClaimsData.setUsername(user.getUsername());
+        jwtClaimsData.setRole(user.getRole());
         map.put("token", jwtService.generateToken(jwtClaimsData));
 
         return map;
