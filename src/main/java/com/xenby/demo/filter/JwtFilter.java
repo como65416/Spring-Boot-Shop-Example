93780aea79cd8e906b5d832bc6bde4c16194a006
comoco <como65416@gmail.com>
2021-09-26 23:49:21 +0800

    fix login api still check jwt token issue

--- src/main/java/com/xenby/demo/filter/JwtFilter.java
+++ src/main/java/com/xenby/demo/filter/JwtFilter.java
@@ -5,48 +5,42 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.xenby.demo.exception.ForbiddenException;
 import com.xenby.demo.exception.UnauthorizedException;
 import com.xenby.demo.model.TokenUserDetails;
 import com.xenby.demo.service.JwtService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-@Component
 public class JwtFilter extends OncePerRequestFilter {
-    @Autowired
-    JwtService jwtService;
+    protected JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         // 提供的 token 無效
-        try {
-            final String requestTokenHeader = request.getHeader("Authorization");
-            if (requestTokenHeader != null) {
-                TokenUserDetails tokenUserDetails = jwtService.loadUserDetailByToken(requestTokenHeader.substring(7));
-                // 紀錄登入資料
-                ArrayList<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + tokenUserDetails.getRole()));
-                PreAuthenticatedAuthenticationToken authentication =
-                        new PreAuthenticatedAuthenticationToken(tokenUserDetails, null, grantedAuthorities);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        final String requestTokenHeader = request.getHeader("Authorization");
+        if (requestTokenHeader == null) {
+            throw new UnauthorizedException("token not validated");
         }
+
+        TokenUserDetails tokenUserDetails = this.jwtService.loadUserDetailByToken(requestTokenHeader.substring(7));
+        // 紀錄登入資料
+        ArrayList<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + tokenUserDetails.getRole()));
+        PreAuthenticatedAuthenticationToken authentication =
+                new PreAuthenticatedAuthenticationToken(tokenUserDetails, null, grantedAuthorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
     }
