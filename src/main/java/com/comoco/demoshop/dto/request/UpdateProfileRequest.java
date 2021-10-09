df67dc0124b8c6525f558d9d5d31893e49c85b67
comoco <como65416@gmail.com>
2021-10-09 18:26:23 +0800

    update profile request add validate

--- src/main/java/com/comoco/demoshop/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/comoco/demoshop/dto/request/UpdateProfileRequest.java
@@ -2,9 +2,15 @@ package com.comoco.demoshop.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class UpdateProfileRequest {
+    @NotBlank(message = "Name is required")
     private String name;
 
+    @Email
+    @NotBlank(message = "Email is required")
     private String email;
 }
